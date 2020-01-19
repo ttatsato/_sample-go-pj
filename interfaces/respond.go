3a83c2291d75892140efc7ecd847358f840f8df7
ttatsato <t-satoh@alby.co.jp>
2020-01-19 19:26:26 +0900

    add: read処理を追加

--- interfaces/respond.go
+++ interfaces/respond.go
@@ -1,19 +1,15 @@
 package interfaces
 
 import (
-	"log"
-	"os"
+	"encoding/json"
+	"fmt"
 )
 
-// printDebugf behaves like log.Printf only in the debug env
-func printDebugf(format string, args ...interface{}) {
-	if env := os.Getenv("GO_SERVER_DEBUG"); len(env) != 0 {
-		log.Printf("[DEBUG] "+format+"\n", args...)
+func convertMapToJsonString(src interface{}) string {
+	bytes, err := json.Marshal(src)
+	if err != nil {
+		fmt.Println("JSON marshal error: ", err)
+		return ""
 	}
+	return string(bytes)
 }
-
-type ApiErrorResponse struct {
-	Status int `json:status`
-	Message string `json:message`
-}
-
