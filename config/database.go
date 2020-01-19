0d49dccb51a81879a83e6b9156f19fda78bb5492
ttatsato <t-satoh@alby.co.jp>
2020-01-19 15:47:00 +0900

    fix: DBに接続できなかった事象を修正

--- config/database.go
+++ config/database.go
@@ -17,17 +17,18 @@ type ConfigDB struct {
 	Dbname   string
 }
 
-var config = ConfigDB{
-	User: os.Getenv("MYSQL_USER"),
-	Password: os.Getenv("MYSQL_PASSWORD"),
-	Host: os.Getenv("MYSQL_HOSTNAME"),
-	Port: os.Getenv("MYSQL_PORT"),
-	Dbname: os.Getenv("MYSQL_DATABASE"),
-}
-
 // ConnectDB returns initialized gorm.DB
 func ConnectDB() (*gorm.DB, error) {
+	config := ConfigDB{
+		User: os.Getenv("MYSQL_USER"),
+		Password: os.Getenv("MYSQL_PASSWORD"),
+		Host: os.Getenv("MYSQL_HOSTNAME"),
+		Port: os.Getenv("MYSQL_PORT"),
+		Dbname: os.Getenv("MYSQL_DATABASE"),
+	}
+	log.Println(config)
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", config.User, config.Password, config.Host, config.Port, config.Dbname)
+	log.Println(dsn)
 	db, err := gorm.Open("mysql", dsn)
 	if err != nil {
 		log.Println(err)
